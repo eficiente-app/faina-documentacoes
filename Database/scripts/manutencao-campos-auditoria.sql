641e1332a5053a8fc85554df70fdd25fbb4c376b
DanielAraujo-dac <danieldearaujocosta@gmail.com>
2020-11-30 20:24:07 -0400

    Adiciona script de automação

---
+++ Database/scripts/manutencao-campos-auditoria.sql
@@ -0,0 +1,57 @@
+    /* Criar Campos */
+SELECT CONCAT('ALTER TABLE ', tables.table_name,' ADD COLUMN ', script_coluna, ';') as script
+  FROM information_schema.tables
+ INNER
+  JOIN
+     ( SELECT 1 as id, 'incluido_id' AS coluna, 'incluido_id INTEGER' AS script_coluna
+        UNION ALL
+       SELECT 2 as id, 'incluido_em' AS coluna, 'incluido_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP' AS script_coluna
+        UNION ALL
+       SELECT 3 as id, 'alterado_id' AS coluna, 'alterado_id INTEGER NULL DEFAULT NULL' AS script_coluna
+        UNION ALL
+       SELECT 4 as id, 'alterado_em' AS coluna, 'alterado_em TIMESTAMP NULL DEFAULT NULL' AS script_coluna
+        UNION ALL
+       SELECT 5 as id, 'excluido_id' AS coluna, 'excluido_id INTEGER NULL DEFAULT NULL' AS script_coluna
+        UNION ALL
+       SELECT 6 as id, 'excluido_em' AS coluna, 'excluido_em TIMESTAMP NULL DEFAULT NULL' AS script_coluna
+     ) as colunas
+    ON colunas.coluna is not null
+  LEFT
+  JOIN information_schema.columns as colunas_db
+    ON colunas_db.table_schema = tables.table_schema
+   AND colunas_db.table_name = tables.table_name
+   AND colunas_db.column_name = colunas.coluna
+ WHERE tables.table_schema = 'faina'
+   AND colunas_db.table_name is null
+ ORDER
+    BY tables.table_name
+     , colunas.id;
+     
+    /* Remover Campos */
+SELECT CONCAT('ALTER TABLE ', tables.table_name,' DROP ', coluna, ';') as script
+  FROM information_schema.tables
+ INNER
+  JOIN
+     ( SELECT 1 as id, 'incluido_id' AS coluna
+        UNION ALL
+       SELECT 2 as id, 'incluido_em' AS coluna
+        UNION ALL
+       SELECT 3 as id, 'alterado_id' AS coluna
+        UNION ALL
+       SELECT 4 as id, 'alterado_em' AS coluna
+        UNION ALL
+       SELECT 5 as id, 'excluido_id' AS coluna
+        UNION ALL
+       SELECT 6 as id, 'excluido_em' AS coluna
+     ) as colunas
+    ON colunas.coluna is not null
+  LEFT
+  JOIN information_schema.columns as colunas_db
+    ON colunas_db.table_schema = tables.table_schema
+   AND colunas_db.table_name = tables.table_name
+   AND colunas_db.column_name = colunas.coluna
+ WHERE tables.table_schema = 'faina'
+   AND colunas_db.table_name is not null
+ ORDER
+    BY tables.table_name
+     , colunas.id;
